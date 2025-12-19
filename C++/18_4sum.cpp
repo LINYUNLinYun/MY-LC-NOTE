d7108d0cffd7342bd9b57b253d0af2ecbc42bb7e
linyun <1828951982@qq.com>
2025-12-19 16:00:08 +0800

    251218 every day practice

--- C++/18_4sum.cpp
+++ C++/18_4sum.cpp
@@ -10,24 +10,48 @@ public:
         vector<vector<int>> result;
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        unordered_map<int, pair<int,int>> hashTable;
-        for(int i = 0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1]){
+        //很难绷的是这题和三数之和一个方法
+        // int first = 0;
+        for(int first = 0;first < n - 3;first++){
+            if(first>0 && nums[first] == nums[first-1]){
                 continue;
             }
-            for(int j = i+1;j<n;j++){
-                if(j>i+1 && nums[j] == nums[j-1]){
+            // 剪枝
+            if((long)nums[first]+nums[first+1]+nums[first+2]+nums[first+3] > (long)target){
+                break;
+            }
+            if((long)nums[first]+nums[n-1]+nums[n-2]+nums[n-3] < (long)target){
+                continue;
+            }
+            for(int second = first+1;second<n-2;second++){
+                if(second>first+1 && nums[second] == nums[second - 1]){
                     continue;
                 }
-                hashTable.insert({nums[i]+nums[j], {i,j}});
-            }
-        } 
-        for(auto iter = hashTable.begin();iter!=hashTable.end();){
-            for(auto iter2 = ++iter;iter2!=hashTable.end();iter2++){
-                if(iter->first+iter2->first == target){
-                    if(nums[iter->second.first] != nums[iter2->second.first] && nums[iter->second.first] != nums[iter2->second.second]
-                    && nums[iter->second.second] != nums[iter2->second.first] && nums[iter->second.second] != nums[iter2->second.second]){
-                        result.push_back({nums[iter->second.first],nums[iter->second.second],nums[iter2->second.first],nums[iter2->second.second]});
+                // 剪枝
+                if((long)nums[first]+nums[second]+nums[second+1]+nums[second+2] > (long)target){
+                    break;
+                }
+                if((long)nums[first]+nums[second]+nums[n-1]+nums[n-2] < (long)target){
+                    continue;
+                }
+                int fourth = n -1;
+                for(int third = second + 1;third<n-1;third++){
+                    if(third>second+1 && nums[third] == nums[third-1]){
+                        continue;
+                    }
+                    // 剪枝
+                    if((long)nums[first]+nums[second]+nums[third]+nums[third] > (long)target){
+                        continue;
+                    }
+                    long target_new = (long)target - nums[first] - nums[second]- nums[third];
+                    while(third < fourth && nums[fourth] >target_new ){
+                        fourth--;
+                    }
+                    if(third == fourth){
+                        break;
+                    }
+                    if(target_new == nums[fourth]){
+                        result.push_back({nums[first],nums[second],nums[third],nums[fourth]});
                     }
                 }
             }
