a2cf7c9a5c9f0fa142650ecb414b8acf82d6987c
linyun <1828951982@qq.com>
2026-03-02 21:15:15 +0800

    20260302 everyday prctice

--- C++/459_repeated_substring_pattern.cpp
+++ C++/459_repeated_substring_pattern.cpp
@@ -3,41 +3,65 @@
 #include <unordered_map>
 #include <algorithm> 
 using namespace std;
-
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int n = s.size();
-        bool isRepeated = false;
-        for(int i =0;i<n/2;i++){
-            string s1 = s.substr(0,i+1);
-            string s2 = s.substr(i+1,i+1);
-            if(s1 == s2){
-                // break;
-                int k = s1.size();
-                if(n%k!=0){
-                    continue;
-                }
-                isRepeated = true;
-                for(int j = 0;j<n/k;j++){
-                    string temp = s.substr(k*j, k);
-                    // cout<<temp<<endl;
-                    if(temp!=s1){
-                        isRepeated = false;
-                    }
-                }
-                if(isRepeated){
-                    break;
-                }
-
-            }
-            else{
+        bool result = false;
+        for(int i = 1; i <= n/2; i++){
+            if(n%i!=0){
                 continue;
             }
-        }      
-        return isRepeated;
+            bool match = true;
+            for(int j = i; j<n;j++){
+                if(s[j]!=s[j-i]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match == true){
+                result = match;
+                break;
+            }
+        }
+        return result;
     }
 };
+//以下是垃圾代码
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         int n = s.size();
+//         bool isRepeated = false;
+//         for(int i =0;i<n/2;i++){
+//             string s1 = s.substr(0,i+1);
+//             string s2 = s.substr(i+1,i+1);
+//             if(s1 == s2){
+//                 // break;
+//                 int k = s1.size();
+//                 if(n%k!=0){
+//                     continue;
+//                 }
+//                 isRepeated = true;
+//                 for(int j = 0;j<n/k;j++){
+//                     string temp = s.substr(k*j, k);
+//                     // cout<<temp<<endl;
+//                     if(temp!=s1){
+//                         isRepeated = false;
+//                     }
+//                 }
+//                 if(isRepeated){
+//                     break;
+//                 }
+
+//             }
+//             else{
+//                 continue;
+//             }
+//         }      
+//         return isRepeated;
+//     }
+// };
 
 
 int main(int argc, char const *argv[]) {
